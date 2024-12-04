afcec68a8cc8d664f19c8b2c16c1b2973213bb53
loickcherimont <loickcherimont@gmail.com>
2024-12-04 22:20:34 +0100

    Create a static class to handle CRUD operations

--- src/main/java/org/eclipse/rest/data/Students.java
+++ src/main/java/org/eclipse/rest/data/Students.java
@@ -4,7 +4,6 @@
  */
 package org.eclipse.rest.data;
 
-//import java.util.ArrayList;
 import java.util.HashMap;
 import org.eclipse.rest.beans.Student;
 
@@ -12,19 +11,42 @@ import org.eclipse.rest.beans.Student;
  *
  * @author loickcherimont
  */
+
+// TODO : handle errors
+
 public class Students {
 
-    private final HashMap<Integer, Student> students;
+    private static HashMap<Integer, Student> all;
 
-    public Students() {
-        students = new HashMap<>();
+    // Create an Hash Map of students
+    public static void generateAll() {
+        HashMap<Integer, Student> students = new HashMap<>();
         students.put(1, new Student(1, "John", "Doe", 24));
         students.put(2, new Student(2, "Jane", "Richards", 20));
         students.put(3, new Student(3, "Nicolas", "Edd", 22));
+        all = students;
     }
-    
-    // Util for later
-    public HashMap<Integer, Student> getStudents() {
-        return students;
+
+    public static HashMap<Integer, Student> getStudents() {
+        return all;
+    }
+
+    public static void deleteById(int id) {
+        all.remove(id);
+        
+        // TODO: Handle cases wherer user removes an unexisting ID
+    }
+
+    public static void add(String firstName, String lastName, int age) {
+        Integer id = Students.getStudents().size() + 1;
+        Student student = new Student(id, firstName, lastName, age);
+        all.put(id, student);
+        // TODO: Handle cases wherer user creates an existing Student
+    }
+
+    public static void modifyById(int id, String firstName, String lastName, int age) {
+        Student student = new Student(id, firstName, lastName, age);
+        all.put(id, student);
+        // TODO: Handle cases wherer user update an existing Student
     }
 }
